bfffa9dc693786f4fba02faa2a536ec08f2c4891
leonardo miraglia <leo.mi.me@gmail.com>
2018-01-12 00:44:23 -0200

    Creating counter without redux

---
+++ counter-react/src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React  from 'react'
+
+import CounterContainer from './components/counter'
+
+import './css/style.css'
+
+const App = () => (
+  <CounterContainer />
+)
+
+export default App
